7543e21dbc535bffc5a7cc26527c46fbd60a80c8
abishek2702 <68456880+abishek2702@users.noreply.github.com>
2026-05-11 20:20:36 -0600

    Implement contact saving feature
    Added functionality to save contact details to Firestore.

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+
 console.log("Edward CRM Loaded");
-console.log(db);
+
+const saveBtn = document.getElementById("saveContact");
+
+saveBtn.addEventListener("click", async () => {
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const stage = document.getElementById("stage").value;
+
+  try {
+
+    await addDoc(collection(db, "contacts"), {
+
+      name: name,
+      email: email,
+      phone: phone,
+      stage: stage,
+      createdAt: new Date()
+
+    });
+
+    alert("Contact Saved!");
+
+    console.log("Saved Successfully");
+
+  } catch(error){
+
+    console.error(error);
+
+  }
+
+});
